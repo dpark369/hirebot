57d9dde5d0a2252b4401c2cb88be6f2980106476
dpark369 <dpark369@gmail.com>
2021-06-17 15:29:28 -0400

    Fetching users from jsonplaceholder, deleted robots array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchField: '',
 		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => this.setState({ robots: users }));
 	}
 	onSearch = (event) => {
 		this.setState({ searchField: event.target.value });
@@ -21,13 +26,17 @@ class App extends Component {
 				.toLowerCase()
 				.includes(this.state.searchField.toLowerCase());
 		});
-		return (
-			<div className='tc'>
-				<h1 className='f1'>HireBot</h1>
-				<SearchBox searchChange={this.onSearch} />
-				<CardList robots={filteredRobots} />
-			</div>
-		);
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>;
+		} else {
+			return (
+				<div className='tc'>
+					<h1 className='f1'>HireBot</h1>
+					<SearchBox searchChange={this.onSearch} />
+					<CardList robots={filteredRobots} />
+				</div>
+			);
+		}
 	}
 }
 
