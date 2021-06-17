9b90e2728523546bc1a236cb58ce398b17af093b
dpark369 <dpark369@gmail.com>
2021-06-17 16:14:19 -0400

    changed the size of the cards

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Card(props) {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img src={`https://robohash.org/${props.id}?200x200`} alt='robot' />
+		<div className='tc bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img src={`https://robohash.org/${props.id}?size=300x300`} alt='robot' />
 			<div>
 				<h2>{props.name}</h2>
 				<p>{props.email}</p>
