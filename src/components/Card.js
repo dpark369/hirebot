401db2f9edbf86f62014214b6d15580a1ef36177
Dan Park <dpark369@gmail.com>
2021-10-04 17:15:26 -0400

    Changed name of the state so it's more intuitive

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,13 @@ import './Card.css';
 
 function Card(props) {
 	// Used to flip card on click
-	const [active, setActive] = useState(false);
+	const [flip, setFlip] = useState(false);
 	const clickHandler = () => {
-		setActive(!isActive);
+		setFlip(!flip);
 	};
 	return (
 		<div className='flip-card tc dib br3 ma2 grow bw2' onClick={clickHandler}>
-			<div className={`flip-card-inner ${active ? 'clicked' : null}`}>
+			<div className={`flip-card-inner ${flip ? 'clicked' : null}`}>
 				<div className='flip-card-front'>
 					<img
 						src={`https://robohash.org/${props.id}?size=300x300`}
